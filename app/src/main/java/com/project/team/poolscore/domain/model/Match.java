e6d633025c9612b3f43d82f9354ea35ad84499b3
Elton Leite <borelanjo@gmail.com>
2018-02-26 21:59:29 -0300

    Adiciona método para configuração do jogo

--- app/src/main/java/com/project/team/poolscore/domain/model/Match.java
+++ app/src/main/java/com/project/team/poolscore/domain/model/Match.java
@@ -1,5 +1,7 @@
 package com.project.team.poolscore.domain.model;
 
+import android.text.format.Time;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -12,24 +14,80 @@ public class Match {
     private Player playerOne;
     private Player playerTwo;
     private List<Ball> availableBalls;
+    private Player currentPlayer;
     private Player winner;
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
+    private Time startTime;
+    private Time endTime;
 
     public Match() {
     }
 
     public Match(Player playerOne, Player playerTwo, List<Ball> availableBalls) {
         this.playerOne = playerOne;
+        this.currentPlayer = playerOne;
         this.playerTwo = playerTwo;
         this.availableBalls = availableBalls;
-        this.startTime = LocalDateTime.now();
+        this.startTime = new Time(Time.getCurrentTimezone());
     }
 
     public Match(Player winner, LocalDateTime endTime) {
         this.winner = winner;
-        this.endTime = LocalDateTime.now();
+        this.endTime = new Time(Time.getCurrentTimezone());
     }
 
+    public Player getPlayerOne() {
+        return playerOne;
+    }
 
+    public void setPlayerOne(Player playerOne) {
+        this.playerOne = playerOne;
+    }
+
+    public Player getPlayerTwo() {
+        return playerTwo;
+    }
+
+    public void setPlayerTwo(Player playerTwo) {
+        this.playerTwo = playerTwo;
+    }
+
+    public List<Ball> getAvailableBalls() {
+        return availableBalls;
+    }
+
+    public void setAvailableBalls(List<Ball> availableBalls) {
+        this.availableBalls = availableBalls;
+    }
+
+    public Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public void setCurrentPlayer(Player currentPlayer) {
+        this.currentPlayer = currentPlayer;
+    }
+
+    public Player getWinner() {
+        return winner;
+    }
+
+    public void setWinner(Player winner) {
+        this.winner = winner;
+    }
+
+    public Time getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Time startTime) {
+        this.startTime = startTime;
+    }
+
+    public Time getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Time endTime) {
+        this.endTime = endTime;
+    }
 }
